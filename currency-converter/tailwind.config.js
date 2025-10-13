071d7c08fe0c523c0a52352e3873e5e45d6f7e40
Michael Achumba <michaelachumba@gmail.com>
2025-10-13 19:20:22 +0100

    Add tailwind.config.js and base index.css

--- currency-converter/tailwind.config.js
+++ currency-converter/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50:  '#f0f9ff',
+          100: '#e0f2fe',
+          500: '#0ea5e9',
+        }
+      }
+    },
   },
   plugins: [],
 }
