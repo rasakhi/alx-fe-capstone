e12efd812388c68668d908f84121fabcea0becbb
Michael Achumba <michaelachumba@gmail.com>
2025-10-19 20:37:28 +0100

    Add router setup and page routes in App.jsx

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Converter from "./pages/Converter";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex-grow bg-gray-50">
-        <Converter />
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Navbar />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
